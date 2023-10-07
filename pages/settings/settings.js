19a27700afec5ba5f75ce94462ffdca42c8c7602
Florian MORENA <93870523+Kactus83@users.noreply.github.com>
2023-10-07 23:46:38 +0200

    basic theming OK

--- pages/settings/settings.js
+++ pages/settings/settings.js
@@ -3,14 +3,14 @@ document.addEventListener("DOMContentLoaded", () => {
     const savedTheme = localStorage.getItem("theme");
     const savedColor = localStorage.getItem("color");
 
+    if(savedColor) {
+        document.body.classList.remove("yellow-theme", "green-theme", "blue-theme", "red-theme", "purple-theme");
+        document.body.classList.add(savedColor);
+    }
+
     if(savedTheme) {
         document.body.classList.remove("dark-theme", "light-theme");
         document.body.classList.add(savedTheme);
-    }
-
-    if(savedColor) {
-        document.body.classList.remove("yellow-theme", "green-theme", "blue-theme", "red-theme", "purple-theme");
-        document.body.classList.add(savedColor + "-theme");
     }
 });
 
