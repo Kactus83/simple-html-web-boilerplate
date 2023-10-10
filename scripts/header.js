1f0a575a6d64de2ed70a91d97005c7418ed94c93
Florian MORENA <93870523+Kactus83@users.noreply.github.com>
2023-10-10 08:16:08 +0200

    Add animations for SVG Logo's parts

--- scripts/header.js
+++ scripts/header.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     var header = document.createElement("header");
 
-    var svgCode = `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="auto" height="100%" viewBox="0 0 300 300">
+    var svgCode = `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="auto" height="100%" viewBox="0 0 300 300" class="svg-logo">
 
     <title>SVG Logo</title>
     <desc>Designed for the SVG Logo Contest in 2006 by Harvey Rayner, and adopted by W3C in 2009. It is available under the Creative Commons license for those who have an SVG product or who are using SVG on their site.</desc>
@@ -34,7 +34,7 @@ document.addEventListener("DOMContentLoaded", function() {
      
   
      <defs>
-       <g id="SVG" fill="var(--primary-title-color)" transform="scale(2) translate(20,79)">
+       <g id="SVG" class="path-svg" transform="scale(2) translate(20,79)">
           <path id="S" d="M 5.482,31.319 C2.163,28.001 0.109,23.419 0.109,18.358 C0.109,8.232 8.322,0.024 18.443,0.024 C28.569,0.024 36.782,8.232 36.782,18.358 L26.042,18.358 C26.042,14.164 22.638,10.765 18.443,10.765 C14.249,10.765 10.850,14.164 10.850,18.358 C10.850,20.453 11.701,22.351 13.070,23.721 L13.075,23.721 C14.450,25.101 15.595,25.500 18.443,25.952 L18.443,25.952 C23.509,26.479 28.091,28.006 31.409,31.324 L31.409,31.324 C34.728,34.643 36.782,39.225 36.782,44.286 C36.782,54.412 28.569,62.625 18.443,62.625 C8.322,62.625 0.109,54.412 0.109,44.286 L10.850,44.286 C10.850,48.480 14.249,51.884 18.443,51.884 C22.638,51.884 26.042,48.480 26.042,44.286 C26.042,42.191 25.191,40.298 23.821,38.923 L23.816,38.923 C22.441,37.548 20.468,37.074 18.443,36.697 L18.443,36.692 C13.533,35.939 8.800,34.638 5.482,31.319 L5.482,31.319 L5.482,31.319 Z"/>
   
           <path id="V" d="M 73.452,0.024 L60.482,62.625 L49.742,62.625 L36.782,0.024 L47.522,0.024 L55.122,36.687 L62.712,0.024 L73.452,0.024 Z"/>
@@ -43,10 +43,10 @@ document.addEventListener("DOMContentLoaded", function() {
         </g>
      </defs>
   
-     <path id="base" fill="var(--primary-background-shadow-color)" d="M8.5,150 H291.5 V250 C291.5,273.5 273.5,291.5 250,291.5 H50 C26.5,291.5 8.5,273.5 8.5,250 Z"/>
-     <g stroke-width="38.0086" stroke="var(--tertiary-background-color)">
+     <path id="base" class="path-base" d="M8.5,150 H291.5 V250 C291.5,273.5 273.5,291.5 250,291.5 H50 C26.5,291.5 8.5,273.5 8.5,250 Z"/>
+     <g stroke-width="38.0086" class="path-star-stroke">
        <g id="svgstar" transform="translate(150, 150)">
-         <path id="svgbar" fill="var(--tertiary-text-color)" d="M-84.1487,-15.8513 a22.4171,22.4171 0 1 0 0,31.7026 h168.2974 a22.4171,22.4171 0 1 0 0,-31.7026 Z"/>
+         <path id="svgbar" class="path-star" d="M-84.1487,-15.8513 a22.4171,22.4171 0 1 0 0,31.7026 h168.2974 a22.4171,22.4171 0 1 0 0,-31.7026 Z"/>
          <use xlink:href="#svgbar" transform="rotate(45)"/>
          <use xlink:href="#svgbar" transform="rotate(90)"/>
          <use xlink:href="#svgbar" transform="rotate(135)"/>
